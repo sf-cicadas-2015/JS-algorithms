298bdc34d6c56234d524943b9218c485ca1ec819
davidbison <davidbisonhubert@gmail.com>
2015-10-07 11:33:33 -0700

    Add first attempt at primes function pseudocode

--- 3-largest-prime-factor/largest-prime-factor.js
+++ 3-largest-prime-factor/largest-prime-factor.js
@@ -18,8 +18,26 @@
   // Return array
 // End function
 
+// Primes function accepts an array of integers as its argument, and returns an array of prime numbers
+  // Create an empty array called primes
+  // If the first element of the array is 2
+    // Do stuff... handle later because test case is odd
+  // Else
+    // Store first element into primes array
+    // Loop through input array starting at index 1
+      // Loop through prime factors array starting at index 0
+        // If input array element is evenly divisible by an integer in the prime array
+          // Do nothing
+        // Else
+          // Add input element to primes array
+        //
+      //
+    //
+  //
+// end primes function
 
-var factors = function(integer) {
+
+function factors(integer) {
   factors = [];
   var limit = Math.floor(Math.sqrt(integer));
   console.log(limit);
@@ -38,9 +56,19 @@ var factors = function(integer) {
         factors.push(i);
       };
     console.log("This test should print.");
-    return(factors);
+    return factors;
   };
 };
 
 
-factors(13195);
+function primes(integers) {
+  console.log("This test should print.");
+
+};
+
+
+console.log("Testing factors function...");
+var factors = factors(13195);
+console.log(factors);
+console.log("Testing primes function...");
+primes(factors);
